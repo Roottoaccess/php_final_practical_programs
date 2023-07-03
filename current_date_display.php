0143212b556474535d598766cb5d2dd64c914ba7
NetworkCrab <network123crab@gmail.com>
2023-07-03 19:21:46 +0530

    current_date_information_display

---
+++ current_date_display.php
@@ -0,0 +1,6 @@
+<?php
+$current_date = getdate();
+
+echo "The current weekday: ". $current_date['weekday'] ." The current month: ". $current_date['month'] ." The current month day: ". $current_date['mday'] ." The current year: ". $current_date['year'] ." Current hour: ". $current_date['hours'] ." Current minutes: ". $current_date['minutes'] ." Current seconds ". $current_date['seconds']; 
+
+?>
