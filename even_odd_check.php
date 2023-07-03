b104643c893136a957c49b6347d21339d12054f4
NetworkCrab <network123crab@gmail.com>
2023-07-03 18:41:34 +0530

    even_odd_number

---
+++ even_odd_check.php
@@ -0,0 +1,11 @@
+<?php
+$number = readline("Enter any number: ");
+
+if($number % 2 == 0){
+    echo "The number $number is even number";
+}
+else{
+    echo "The number $number is odd number";
+}
+
+?>
