220fd7a531b8ba054d7a62fe2c13b412133621a8
NetworkCrab <network123crab@gmail.com>
2023-07-03 18:59:46 +0530

    swaping_two_numbers

---
+++ swap_two_numbers.php
@@ -0,0 +1,14 @@
+<?php
+$num1 = readline("Enter the first number: ");
+$num2 = readline("Enter the second number: ");
+
+$num3 = 0;
+
+$num3 = $num1;
+$num1 = $num2;
+$num2 = $num3;
+
+echo "The first number is:$num1 and the second number is:$num2";
+
+
+?>
